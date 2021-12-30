477dc6e156b9681b91410ed54561d2378a4461c2
George Serban <georgeitprojects@gmail.com>
2021-12-30 10:21:39 -0800

    changed subnets to private

--- efs.tf
+++ efs.tf
@@ -9,14 +9,14 @@ resource "aws_security_group" "wp_efs_sg" {
     from_port   = 2049
     to_port     = 2049
     protocol    = "tcp"
-    cidr_blocks = [module.vpc.vpc_cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = [module.vpc.vpc_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 }
@@ -27,12 +27,18 @@ resource "aws_efs_file_system" "efs_wp" {
   performance_mode = "generalPurpose"
   throughput_mode  = "bursting"
   encrypted        = false
+
+  tags = {
+    Name = "wp-storage"
+  }
 }
 
 # Create the mount target in 3 different subnets
 resource "aws_efs_mount_target" "wp_mnt_target" {
-  count           = length(module.vpc.public_subnets)
+  count           = length(module.vpc.private_subnets)
   file_system_id  = aws_efs_file_system.efs_wp.id
-  subnet_id       = module.vpc.public_subnets[count.index]
-  security_groups = [aws_security_group.wp_efs_sg.id]
+  subnet_id       = module.vpc.private_subnets[count.index]
+  security_groups = [aws_security_group.ecs_sg.id]
+
+  depends_on = [aws_efs_file_system.efs_wp]
 }
